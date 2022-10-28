b379cc3d903834455e5d932414dc66652d12550d
camibarrera16@hotmail.com <camibarrera16@hotmail.com>
2022-10-27 21:55:15 -0500

    update

---
+++ back/config/database.js
@@ -0,0 +1,15 @@
+const mongoose=require("mongoose"); 
+
+const connectDatabase = () => {
+    mongoose.connect(process.enviroment.DB_LOCAL_UR, {
+        userNewUrlParser: true,
+        useUnifiedTopology: true
+
+    }), then (con => {
+        console.log(`Base de datos conectada con el servidor: ${con.connection.host}`)
+
+    })
+
+}
+
+module.exports=connectDatabase; 
