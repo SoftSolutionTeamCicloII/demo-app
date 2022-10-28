9d2b0ed39bb610cc3429393e836172a22d2cc9b8
chapersi@gmail.com <chapersi@gmail.com>
2022-10-27 22:26:33 -0500

    configuración mogodb

---
+++ src/back/routes/server.js
@@ -0,0 +1,17 @@
+const app=require("./app")
+const connectDatabase = require("../config/database");
+
+//setear el archivo de configuracion
+const dotenv=require("dotenv"); 
+dotenv.configr({path: 'back/config/config.env'})
+
+//configurar base de datos
+connectDatabase
+
+//llamemos al server
+const sever=app.listen(process.env.PORT, () => {
+
+    console.log('Servidor iniciado en el puerto: ${process.env.PORT} en modo: ${process.env.NODE_ENV}')
+
+})
+    
