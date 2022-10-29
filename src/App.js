a14b03e5f0f0beaf180f1eb7754b3271546bcdea
Nataly Ramos <jnramosm@gmail.com>
2022-10-29 13:39:33 -0500

    actualización

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Header } from "./componentes/Header";
-import { ProductosLista } from "./componentes/Productos/index";
+import {Header} from "./componentes/Header";
+import {ProductosLista} from "./componentes/Productos/index";
 import 'boxicons';
 
 function App() {
