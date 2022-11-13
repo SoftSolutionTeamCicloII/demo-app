8219a5f3440262fb34f5a4089df660c5a5597f08
Julian-Tobito <111189694+Julian-Tobito@users.noreply.github.com>
2022-11-12 19:49:23 -0500

    Add files via upload
    Agregar/Eliminar Producto

---
+++ demo-app/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Header } from "./componentes/Header";
+import 'boxicons';
+import {BrowserRouter as Router} from "react-router-dom";
+import {Paginas} from "./componentes/Paginas";
+
+function App() {
+    return (
+       <div className ="App">
+        <Router>
+          <Header />
+          <Paginas />
+        </Router> 
+
+       </div>
+    );
+}
+
+export default App;
